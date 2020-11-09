865aafc6660d5fbdd8113611238bcfc2f81debf3
neenbeannnn <hmn.nini1970@gmail.com>
2020-11-08 16:09:39 -0800

    save work

--- src/pages/Homepage/About-us-Preview.tsx
+++ src/pages/Homepage/About-us-Preview.tsx
@@ -1,44 +1,53 @@
-import React from "react";
-import styled, { css } from "styled-components";
-import Preview from "./Preview";
-import Img from  "../../../static/about-us.jpg"
-import Button from "../Button";
+import React from "react"
+import styled, { css } from "styled-components"
+import Preview from "./Preview"
+import Img from "../../../static/about-us.jpg"
+import Button from "../Button"
 
 const Info = styled.div`
-    background: green;
-    flex-basis: 50%;
-    order: 1;
+  background: green;
+  flex-basis: 50%;
+  order: 1;
 `
 
 const Title = styled.h1`
-    font-size: 3vw;
+  font-size: 3vw;
 `
 
 const Text = styled.p`
-    font-size: 1.5vw;
+  font-size: 1.5vw;
 `
 
 const Image = styled.img`
-    height: 100%;
+  height: 100%;
 `
 const ImageWrapper = styled.div`
-    background: red;
-    height: 100%;
-    order: 2;
+  background: red;
+  height: 100%;
+  order: 2;
 
-    display: flex;
-    flex-basis: 50%;
-    align-content: stretch;
-    justify-content: stretch;
+  display: flex;
+  flex-basis: 50%;
+  align-content: stretch;
+  justify-content: stretch;
 `
 
 export default function About_us_Preview() {
-    return <Preview>
-        <Info>
-            <Title>About Us</Title>
-            <Text>We participate in several competitions, educational outreach programs and member-initied projects. </Text>
-            <center><Button link="about-us">Learn More</Button></center>
-        </Info>
-        <ImageWrapper><Image src={Img} /></ImageWrapper>
+  return (
+    <Preview>
+      <Info>
+        <Title>About Us</Title>
+        <Text>
+          We participate in several competitions, educational outreach programs
+          and member-initied projects.{" "}
+        </Text>
+        <center>
+          <Button link="about-us">Learn More</Button>
+        </center>
+      </Info>
+      <ImageWrapper>
+        <Image src={Img} />
+      </ImageWrapper>
     </Preview>
+  )
 }
