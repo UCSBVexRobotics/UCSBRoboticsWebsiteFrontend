19685172b19d1233e1de48e35be21bcba4d6e5e9
alrudolph <alex3r@me.com>
2020-11-30 09:03:04 -0800

    update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -118,7 +118,7 @@ export default function NavBar() {
         openLoginModal={() => setIsLoginModalOpen(true)}
         openSignupModal={() => setIsSignupModalOpen(true)}
         closeModal={() => setIsForgotPasswordModalOpen(false)}
-        title="Signup"
+        title="Forgot Password"
       />
       <NavBarInnerContainer>
         <Title onClick={() => toTopOfPage()}>
