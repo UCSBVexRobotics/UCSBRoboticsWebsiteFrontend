ade375dbc1f49b9d66fe90862f91a99b48554502
alrudolph <alex3r@me.com>
2021-02-13 18:44:43 -0800

    profile page editable projects

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,6 +5,7 @@ import styled from "styled-components"
 import { Email } from "@styled-icons/material/Email"
 import { Github } from "@styled-icons/boxicons-logos/Github"
 import { LinkedinSquare } from "@styled-icons/boxicons-logos/LinkedinSquare"
+import { Delete } from "@styled-icons/feather/Delete"
 
 const PageContainer = styled.div`
   display: flex;
@@ -86,6 +87,11 @@ const Names = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
+  align-items: center;
+
+  @media (max-width: 580px) {
+    flex-wrap: wrap;
+  }
 `
 
 const Name = styled.h1`
@@ -154,15 +160,15 @@ const Circle = styled.div`
 const ButtonArea = styled.div`
   display: flex;
   flex-direction: row;
-
-  @media (max-width: 500px) {
+  width: 100%;
+  @media (max-width: 580px) {
     flex-wrap: wrap;
   }
 `
 
 const EditButton = styled.div`
   padding: 10px 30px;
-  margin: 5px 10px;
+  margin: 5px ${({ open }) => (open ? "10px" : "0")};
   width: 100%;
   color: white;
   font-size: 20px;
@@ -181,8 +187,9 @@ const EditButton = styled.div`
 
 const Edit = styled.input.attrs(props => ({
   type: "text",
+  contenteditable: "true", // this doesn't work
 }))`
-  height: 30px;
+  min-height: 30px;
   width: 100%;
 
   border-radius: 7px;
@@ -190,7 +197,16 @@ const Edit = styled.input.attrs(props => ({
   margin: 3px;
 
   font-size: 20px;
+  overflow-x: hidden;
+  overflow-y: auto;
 `
+/*
+`
+overflow: auto;
+  background-color: white;
+  color: black;
+`
+*/
 
 const EditableArea = styled.div`
   display: flex;
@@ -199,6 +215,52 @@ const EditableArea = styled.div`
     margin: 5px;
   }
   ${({ editing }) => (editing ? "width: 100%" : null)};
+`
+
+const ContentUser = styled.div`
+  width: ${({ open }) => (open ? "100%" : "auto")};
+`
+const Project = styled.div`
+  border-radius: 10px;
+  background-color: #5478bf;
+  padding: 10px 10px;
+  margin: 10px 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+  overflow: auto;
+`
+const ProjectTitle = styled.h4`
+  margin: 0;
+  font-style: italic;
+`
+const ProjectContent = styled.p`
+  margin: 0;
+  overflow: auto;
+`
+const NameLabel = styled.p`
+  margin: 0;
+  width: 250px;
+`
+
+const NameGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+`
+
+const ProjectDelete = styled(Delete)`
+  height: 30px;
+  cursor: pointer;
+`
+
+const ProjectTitleEditArea = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
 `
 
 export default function Profile() {
@@ -215,6 +277,11 @@ export default function Profile() {
     mRoboChip: 3,
     eRoboChip: 20,
     sRoboChip: 56,
+    projects: [
+      { title: "Project 1", desc: "Lead" },
+      { title: "Project 2", desc: "Did stuff" },
+      { title: "Project 3", desc: "Not sure" },
+    ],
   }
 
   const [shake, setShake] = useState(false)
@@ -240,28 +307,34 @@ export default function Profile() {
       <PageContainer>
         <Content>
           <ContentHeading>
-            <div>
+            <ContentUser open={editOpen}>
               <NameContainer>
                 {editOpen ? (
                   <Names>
-                    <Edit
-                      name="firstName"
-                      defaultValue={formValues.firstName}
-                      onChange={newVal =>
-                        changeFormValue({
-                          firstName: newVal.currentTarget.value,
-                        })
-                      }
-                    />
-                    <Edit
-                      name="lastName"
-                      defaultValue={formValues.lastName}
-                      onChange={newVal =>
-                        changeFormValue({
-                          lastName: newVal.currentTarget.value,
-                        })
-                      }
-                    />
+                    <NameGroup>
+                      <NameLabel>First Name:</NameLabel>
+                      <Edit
+                        name="firstName"
+                        defaultValue={formValues.firstName}
+                        onChange={newVal =>
+                          changeFormValue({
+                            firstName: newVal.currentTarget.value,
+                          })
+                        }
+                      />
+                    </NameGroup>
+                    <NameGroup>
+                      <NameLabel>Last Name:</NameLabel>
+                      <Edit
+                        name="lastName"
+                        defaultValue={formValues.lastName}
+                        onChange={newVal =>
+                          changeFormValue({
+                            lastName: newVal.currentTarget.value,
+                          })
+                        }
+                      />
+                    </NameGroup>
                   </Names>
                 ) : (
                   <Name>
@@ -272,48 +345,52 @@ export default function Profile() {
                   <VerifyEmail shake={shake}>Verify Email</VerifyEmail>
                 ) : null}
               </NameContainer>
-              <Roles>
-                {account.role === "admin" ? <Role>Admin</Role> : null}
-                {
-                  /* We could do role stuff like this, idk */
-                  account.sRoboChip >= 35 ? (
-                    <Role>Software Team Developer</Role>
-                  ) : null
-                }
-              </Roles>
-            </div>
-            <Options>
-              {account.mRoboChip ? (
-                <Tooltip>
-                  <Circle size="40" color="#24e07c">
-                    {account.mRoboChip}
-                  </Circle>
-                  <ToolTipText color="#24e07c">
-                    Mechanical Team RoboChips
-                  </ToolTipText>
-                </Tooltip>
+              {!editOpen ? (
+                <Roles>
+                  {account.role === "admin" ? <Role>Admin</Role> : null}
+                  {
+                    /* We could do role stuff like this, idk */
+                    account.sRoboChip >= 35 ? (
+                      <Role>Software Team Developer</Role>
+                    ) : null
+                  }
+                </Roles>
               ) : null}
-              {account.eRoboChip ? (
-                <Tooltip>
-                  <Circle size="40" color="#fac739">
-                    {account.eRoboChip}
-                  </Circle>
-                  <ToolTipText color="#fac739">
-                    Electrical Team RoboChips
-                  </ToolTipText>
-                </Tooltip>
-              ) : null}
-              {account.sRoboChip ? (
-                <Tooltip>
-                  <Circle size="40" color="#2d6ae3">
-                    {account.sRoboChip}
-                  </Circle>
-                  <ToolTipText color="#2d6ae3">
-                    Software Team RoboChips
-                  </ToolTipText>
-                </Tooltip>
-              ) : null}
-            </Options>
+            </ContentUser>
+            {!editOpen ? (
+              <Options>
+                {account.mRoboChip ? (
+                  <Tooltip>
+                    <Circle size="40" color="#24e07c">
+                      {account.mRoboChip}
+                    </Circle>
+                    <ToolTipText color="#24e07c">
+                      Mechanical Team RoboChips
+                    </ToolTipText>
+                  </Tooltip>
+                ) : null}
+                {account.eRoboChip ? (
+                  <Tooltip>
+                    <Circle size="40" color="#fac739">
+                      {account.eRoboChip}
+                    </Circle>
+                    <ToolTipText color="#fac739">
+                      Electrical Team RoboChips
+                    </ToolTipText>
+                  </Tooltip>
+                ) : null}
+                {account.sRoboChip ? (
+                  <Tooltip>
+                    <Circle size="40" color="#2d6ae3">
+                      {account.sRoboChip}
+                    </Circle>
+                    <ToolTipText color="#2d6ae3">
+                      Software Team RoboChips
+                    </ToolTipText>
+                  </Tooltip>
+                ) : null}
+              </Options>
+            ) : null}
           </ContentHeading>
           <LinkBar>
             <EditableArea editing={editOpen}>
@@ -372,9 +449,57 @@ export default function Profile() {
               ) : null}
             </EditableArea>
           </LinkBar>
+          {formValues.projects.length > 0 || editOpen
+            ? formValues.projects.map((proj, i) => {
+                return (
+                  <Project>
+                    {editOpen ? (
+                      <ProjectTitleEditArea>
+                        <Edit
+                          defaultValue={formValues.projects[i].title}
+                          onChange={newVal => {
+                            formValues.projects[i].title =
+                              newVal.currentTarget.value
+                            changeFormValue({
+                              projects: formValues.projects,
+                            })
+                          }}
+                        />
+                        <ProjectDelete
+                          onClick={() => {
+                            let copy = formValues.projects
+                            copy.splice(i, 1)
+                            changeFormValue({
+                              projects: copy,
+                            })
+                          }}
+                        />
+                      </ProjectTitleEditArea>
+                    ) : (
+                      <ProjectTitle>{proj.title}</ProjectTitle>
+                    )}
+                    {editOpen ? (
+                      <Edit
+                        defaultValue={formValues.projects[i].desc}
+                        onChange={newVal => {
+                          formValues.projects[i].desc =
+                            newVal.currentTarget.value
+                          changeFormValue({
+                            projects: formValues.projects,
+                          })
+                        }}
+                      />
+                    ) : (
+                      <ProjectContent>&nbsp;-&nbsp;{proj.desc}</ProjectContent>
+                    )}
+                  </Project>
+                )
+              })
+            : null}
           <ButtonArea>
             {editOpen ? (
               <EditButton
+                open={editOpen}
                 size="45"
                 color="orange"
                 onClick={() => {
@@ -393,6 +518,7 @@ export default function Profile() {
               </EditButton>
             ) : null}
             <EditButton
+              open={editOpen}
               size="45"
               color="orange"
               onClick={() => {
